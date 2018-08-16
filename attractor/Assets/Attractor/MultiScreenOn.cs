fe9388e614d4f6a19726ab14aa817058d5e8bd24
JF <jfan9988@gmail.com>
2018-08-17 00:06:19 +1000

    added side screen animation

---
+++ attractor/Assets/Attractor/MultiScreenOn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiScreenOn : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("displays connected: " + Display.displays.Length);
+        // Display.displays[0] is the primary, default display and is always ON.
+        // Check if additional displays are available and activate each.
+        if (Display.displays.Length > 1)
+            Display.displays[1].Activate();
+    }
+
+}
