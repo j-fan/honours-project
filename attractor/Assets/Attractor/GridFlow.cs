173987cc1a2f33bebc5175b7e30a693ad4697fe9
jfan <jfan9988@gmail.com>
2018-09-13 00:24:27 +1000

    create procedural rainbow grid files

---
+++ attractor/Assets/Attractor/GridFlow.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  procedural grid adapted from cat-like coding
+ *  https://catlikecoding.com/unity/tutorials/procedural-grid/
+ */
+
+[RequireComponent (typeof(MeshFilter),typeof(MeshRenderer))]
+
+public class GridFlow : MonoBehaviour {
+    public Targets targets;
+    public AudioSource audioSource;
+    public int xSize = 10;
+    public int ySize = 20;
+    public float cellSize = 10;
+
+
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    Vector3 gridOffset;
+
+    // Use this for initialization
+    void Awake () {
+        mesh = GetComponent<MeshFilter>().mesh;
+	}
+	
+    void Start()
+    {
+        gridOffset = GetComponent<Transform>().position;
+        gridOffset = gridOffset - new Vector3(cellSize * 0.5f, 0, cellSize * 0.5f);  //make cells centered
+        updateMesh();
+        makeGrid();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void makeGrid()
+    {
+        //generate geometry & UVs
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Color[] colours = new Color[vertices.Length];
+
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x*cellSize, 0, y*cellSize) + gridOffset;
+                uv[i] = new Vector2((float)x , (float)y );
+                colours[i] = Color.HSVToRGB((float)y/ySize, 1.0f,0.5f) ;
+
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.colors = colours;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+    }
+    void updateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
