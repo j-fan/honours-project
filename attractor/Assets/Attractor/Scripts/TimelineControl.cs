941812a9351b231bafb4c7e3661c19a70629407e
jfan <jfan9988@gmail.com>
2018-11-25 19:54:20 +1100

    add audio input option and auto restart of timeline based on silence

--- attractor/Assets/Attractor/Scripts/TimelineControl.cs
+++ attractor/Assets/Attractor/Scripts/TimelineControl.cs
@@ -7,6 +7,10 @@ using UnityEngine.Playables;
 public class TimelineControl : MonoBehaviour {
 
     PlayableDirector playableDirector;
+    public bool autoRestartEnabled;
+    public float autoRestartDuration = 0; //the amount seconds of silence to trigger restart
+    public BeatsFFT beatsFFT;
+    float silenceTime = 0;
 
 
 	// Use this for initialization
@@ -22,6 +26,33 @@ public class TimelineControl : MonoBehaviour {
             {
                 playableDirector.time = 0;
             }
+        }
+
+        if (autoRestartEnabled)
+        {
+            if (beatsFFT.avgFreq < 0.001f)
+            {
+                silenceTime += Time.deltaTime;
+            }
+            else
+            {
+                silenceTime = 0;
+            }
+            if (silenceTime > autoRestartDuration)
+            {
+                silenceTime = 0;
+                playableDirector.time = 0;
+                print("max silence period reached");
+            }
+        }
+
+    }
+
+    void OnGUI()
+    {
+        if (autoRestartEnabled)
+        {
+            GUI.Box(new Rect(10, 10, 150, 100), "Silence Time: " + Mathf.FloorToInt(silenceTime).ToString());
         }
 
     }
