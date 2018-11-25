74a864ddf629c909cda0359fcacfb80770eac3cd
jfan <jfan9988@gmail.com>
2018-11-25 18:40:04 +1100

    new annual animations

---
+++ attractor/Assets/Attractor/Scripts/SceneControlAnnual.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControlAnnual : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene("annual", LoadSceneMode.Additive);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+}
---
+++ attractor/Assets/Attractor/Scripts/TimelineControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class TimelineControl : MonoBehaviour {
+
+    PlayableDirector playableDirector;
+
+
+	// Use this for initialization
+	void Start () {
+        playableDirector = GetComponent<PlayableDirector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(playableDirector != null)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                playableDirector.time = 0;
+            }
+        }
+
+    }
+}
